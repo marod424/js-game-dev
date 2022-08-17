ae20f8e9d760a61248cf93baec0f102a01cde8fb
matthew <marod424@gmail.com>
2022-08-17 15:45:46 -0400

    state management

--- 8-state-management/script.js
+++ 8-state-management/script.js
@@ -1,3 +1,7 @@
+import Player from './player.js';
+import InputHandler from './input.js';
+import { drawStatusText } from './utils.js';
+
 window.addEventListener('load', function() {
   /** @type {HTMLCanvasElement} */
   const canvas = document.getElementById('canvas1');
@@ -7,4 +11,23 @@ window.addEventListener('load', function() {
 
   const loading = document.getElementById('loading');
   loading.style.display = 'none';
+
+  const player = new Player(canvas.width, canvas.height);
+  
+  const input = new InputHandler();
+  
+  let lastTime = 0;
+
+  function animate(timestamp) {
+    const deltatime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update(input.lastKey);
+    player.draw(ctx, deltatime);
+    drawStatusText(ctx, input, player);
+    requestAnimationFrame(animate);
+  }
+
+  animate(0);
 });
